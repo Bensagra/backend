edbca0f0abd439e41c8063e185cc67cbf68def46
Bensagra <bensagra@gmail.com>
2025-05-27 08:51:42 -0300

    Fix model import paths in app.js for consistency

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ const authApiRouter = require("./routes/authApi");
 const errorHandlers = require("./handlers/errorHandlers");
 
 const { isValidToken } = require("./controllers/authController");
-require("./models/Admin");
-require("./models/Client");
-require("./models/Product");
-require("./models/Lead");
-require("./models/ProcessCSV");
+require("./models/Admin.js");
+require("./models/Client.js");
+require("./models/Product.js");
+require("./models/Lead.js");
+require("./models/ProcessCSV.js");
 require("dotenv").config({ path: ".variables.env" });
 
 // create our Express app
